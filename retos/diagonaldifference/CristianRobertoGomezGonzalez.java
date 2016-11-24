36f37acb4f31e0feff5b55f23b3b57025e8c83a4
root <jemy.dasm@gmail.com>
2016-11-24 11:14:40 -0500

    Añadir descripcion de verybigsum y código histogramas

--- retos/diagonaldifference/CristianRobertoGomezGonzalez.java
+++ retos/diagonaldifference/CristianRobertoGomezGonzalez.java
@@ -3,6 +3,7 @@ package hackerrank.test.retos.diagonaldifference;
 import java.util.Scanner;
 
 public class CristianRobertoGomezGonzalez {
+	
 	public static void main(String []args){
         int tam = 0,suma1=0,suma2=0,resultado;
         
